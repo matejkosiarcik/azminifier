1b55e5b675f2e774a377d61d2dbe818f114544af
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-06-22 12:31:07 +0200

    chore: fix typescript indentation

--- cli/tests/playwright.config.ts
+++ cli/tests/playwright.config.ts
@@ -3,10 +3,10 @@ import os from 'os';
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  testDir: 'tests',
-  testMatch: '*.test.ts',
-  fullyParallel: true,
-  forbidOnly: process.env['CI'] === '1',
-  workers: os.cpus().length * 2,
-  reporter: 'html',
+    testDir: 'tests',
+    testMatch: '*.test.ts',
+    fullyParallel: true,
+    forbidOnly: process.env['CI'] === '1',
+    workers: os.cpus().length * 2,
+    reporter: 'html',
 });
